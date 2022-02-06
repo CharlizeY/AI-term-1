079713be84ac14e6b90ed1b3fd44216eecab56ea
Charlize <54396544+CharlizeY@users.noreply.github.com>
2022-02-06 22:02:31 +0000

    Delete Python Programming directory

--- Python Programming/Python Mock Test/q1/q1.py
+++
@@ -1,7 +0,0 @@
-def odd_even_swap(word):
-    split_word = list(word)
-    for i in range(1, len(split_word)):
-        if (i % 2) != 0:
-            split_word[i-1], split_word[i] = split_word[i], split_word[i-1]
-    word = "".join(split_word)
-    return(word)
